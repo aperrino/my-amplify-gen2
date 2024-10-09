dbff23757519b6cf6e0ba71f4a1afd25bc6c54f1
yeonuk.lim <lim454@naver.com>
2024-10-10 07:18:55 +0900

    update Comments.tsx

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -1,5 +1,4 @@
-// ui
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useCallback } from 'react';
 import {
   Box,
   Button,
@@ -20,39 +19,36 @@ import moment from 'moment';
 const client = generateClient<Schema>();
 
 function NewLineToBr({ children = "" }) {
-  return children.split('\n').reduce(function (arr, line) {
-    return arr.concat(line, <br />);
-  }, []);
+  return children.split('\n').reduce((arr, line) => arr.concat(line, <br />), []);
 }
 
-const NoComment = () => {
-  return (
-    <Box
-      padding={{ bottom: "s" }}
-      fontSize="heading-s"
-      textAlign="center"
-      color="inherit"
-    >
-      <b>No Contents</b>
-    </Box>
-  );
-}
+const NoComment = () => (
+  <Box
+    padding={{ bottom: "s" }}
+    fontSize="heading-s"
+    textAlign="center"
+    color="inherit"
+  >
+    <b>No Contents</b>
+  </Box>
+);
 
 const Comment = ({
   comment,
   activeComment,
   setActiveComment,
+  editCommentApi,
+  deleteCommentApi,
 }) => {
-  const isEditing = activeComment && activeComment.type === "edit" && activeComment.id === comment.id
   const [confirmVisible, setConfirmVisible] = useState(false);
 
-  const deleteHandler = () => {
-    deleteCommentApi(comment.id, comment._version);
+  const deleteHandler = async () => {
+    await deleteCommentApi(comment.id, comment._version);
     setConfirmVisible(false);
   }
 
-  return (
-    isEditing ?
+  if (activeComment && activeComment.type === "edit" && activeComment.id === comment.id) {
+    return (
       <CommentForm
         initText={comment.content}
         classId={comment.classId}
@@ -60,39 +56,44 @@ const Comment = ({
         commentVersion={comment._version}
         activeComment={activeComment}
         setActiveComment={setActiveComment}
-      /> :
-      <>
-        <TextContent>
-          <h4>{comment.owner}</h4>
-          <p>
-            <small>{moment(comment.updatedAt).fromNow()}</small>
-          </p>
-        </TextContent>
+        editCommentApi={editCommentApi}
+      />
+    );
+  }
 
-        <SpaceBetween direction="horizontal" size="xxs">
-          <Button iconName="contact" variant="icon" />
-          <Button iconName="edit" variant="icon" onClick={() => setActiveComment({ id: comment.id, type: "edit" })} />
-          <Button iconName="remove" variant="icon" onClick={() => setConfirmVisible(true)} />
-        </SpaceBetween>
+  return (
+    <>
+      <TextContent>
+        <h4>{comment.owner}</h4>
+        <p>
+          <small>{moment(comment.updatedAt).fromNow()}</small>
+        </p>
+      </TextContent>
 
-        <Modal
-          onDismiss={() => setConfirmVisible(false)}
-          visible={confirmVisible}
-          closeAriaLabel="Close modal"
-          size="small"
-          footer={
-            <Box float="right">
-              <SpaceBetween direction="horizontal" size="xs">
-                <Button variant="link" onClick={() => setConfirmVisible(false)}>Cancel</Button>
-                <Button variant="primary" onClick={deleteHandler}>Confirm</Button>
-              </SpaceBetween>
-            </Box>
-          }
-        >
-          Are you sure to delete the message?
-        </Modal>
-        <NewLineToBr>{comment.content}</NewLineToBr>
-      </>
+      <SpaceBetween direction="horizontal" size="xxs">
+        <Button iconName="contact" variant="icon" />
+        <Button iconName="edit" variant="icon" onClick={() => setActiveComment({ id: comment.id, type: "edit" })} />
+        <Button iconName="remove" variant="icon" onClick={() => setConfirmVisible(true)} />
+      </SpaceBetween>
+
+      <Modal
+        onDismiss={() => setConfirmVisible(false)}
+        visible={confirmVisible}
+        closeAriaLabel="Close modal"
+        size="small"
+        footer={
+          <Box float="right">
+            <SpaceBetween direction="horizontal" size="xs">
+              <Button variant="link" onClick={() => setConfirmVisible(false)}>Cancel</Button>
+              <Button variant="primary" onClick={deleteHandler}>Confirm</Button>
+            </SpaceBetween>
+          </Box>
+        }
+      >
+        Are you sure to delete the message?
+      </Modal>
+      <NewLineToBr>{comment.content}</NewLineToBr>
+    </>
   );
 }
 
@@ -103,28 +104,29 @@ const CommentForm = ({
   commentVersion,
   activeComment,
   setActiveComment,
+  createCommentApi,
+  editCommentApi,
 }) => {
   const [post, setPost] = useState(initText);
   const [alertVisible, setAlertVisible] = useState(false);
 
-  const submitHandler = (event) => {
+  const submitHandler = async (event) => {
     event.preventDefault();
     if (post.replace(/\s/g, '').length > 0) {
       if (activeComment && activeComment.type === "edit") {
-        editCommentApi(commentId, commentVersion, post);
+        await editCommentApi(commentId, commentVersion, post);
         setActiveComment(null);
-      }
-      else {
-        createCommentApi(post, classId);
+      } else {
+        await createCommentApi(post, classId);
         setPost("");
       }
-    }
-    else {
+    } else {
       setAlertVisible(true);
     }
   };
+
   const cancelHandler = () => {
-    activeComment && activeComment.type === "edit" ? setActiveComment(null) : setPost("")
+    activeComment && activeComment.type === "edit" ? setActiveComment(null) : setPost("");
   }
 
   return (
@@ -155,74 +157,92 @@ const CommentForm = ({
       </Form>
     </form>
   );
-}
+};
 
-function Comments(selectedClass) {
-  const [comments, setComments] = useState<Schema["Comment"]["type"][]>([]);
+function Comments({ classId }) {
+  const [comments, setComments] = useState([]);
   const [activeComment, setActiveComment] = useState(null);
 
-  const fetchTodos = async () => {
-    const { data: items, errors } = await client.models.Comment.list();
-    setComments(items);
-  };
+  const fetchComments = useCallback(async () => {
+    const { data: items, errors } = await client.models.Comment.list({
+      filter: { classId: { eq: classId } }
+    });
+    if (errors) {
+      console.error('Error fetching comments:', errors);
+    } else {
+      setComments(items);
+    }
+  }, [classId]);
 
   useEffect(() => {
-    fetchTodos();
-  }, []);
+    fetchComments();
+  }, [fetchComments]);
+
+  const createCommentApi = useCallback(async (post, classId) => {
+    const { errors, data: newComment } = await client.models.Comment.create({
+      classId: classId,
+      content: post,
+      commentVersion: "1",
+    });
+    console.log(newComment);
+    await fetchComments();
+  }, [fetchComments]);
+
+  const editCommentApi = useCallback(async (commentId, commentVersion, post) => {
+    const updatedPost = {
+      id: commentId,
+      content: post,
+      _version: commentVersion,
+    };
+    const { data: updatedComment, errors } = await client.models.Comment.update(updatedPost);
+    console.log(updatedComment);
+    await fetchComments();
+  }, [fetchComments]);
+
+  const deleteCommentApi = useCallback(async (commentId, commentVersion) => {
+    const toBeDeletedTodo = {
+      id: commentId,
+      _version: commentVersion,
+    };
+    const { data: deletedComment, errors } = await client.models.Comment.delete(toBeDeletedTodo);
+    console.log(deletedComment);
+    await fetchComments();
+  }, [fetchComments]);
 
   return (
     <Container header={<Header variant='h3'>Comments</Header>}>
       <Box float='center'>
         <SpaceBetween size="xl">
-          <CommentForm classId={selectedClass.classId} />
+          <CommentForm 
+            classId={classId} 
+            createCommentApi={createCommentApi}
+            editCommentApi={editCommentApi}  
+            activeComment={activeComment} 
+            setActiveComment={setActiveComment}
+          />
           <SpaceBetween size="xs">
-            {
-              comments.length > 0 ? (comments.sort((a, b) => b.createdAt.localeCompare(a.updatedAt)).map(comment =>
-                <Comment
-                  key={comment.id}
-                  comment={comment}
-                  activeComment={activeComment}
-                  setActiveComment={setActiveComment}
-                />
-              )) : <NoComment />
-            }
+            {comments.length > 0 ? (
+              comments
+                .filter(comment => comment.classId === classId)  // classId로 필터링
+                .sort((a, b) => b.createdAt.localeCompare(a.updatedAt))
+                .map(comment => (
+                  <Comment
+                    key={comment.id}
+                    comment={comment}
+                    activeComment={activeComment}
+                    setActiveComment={setActiveComment}
+                    editCommentApi={editCommentApi}
+                    deleteCommentApi={deleteCommentApi}
+                  />
+                ))
+            ) : (
+              <NoComment />
+            )}
           </SpaceBetween>
         </SpaceBetween>
       </Box>
     </Container>
   );
-}
-
-// apis
-async function createCommentApi(post, classId) {
-  const { errors, data: newComment } = await client.models.Comment.create({
-    classId: classId,
-    content: post,
-    commentVersion: "1",
-  })
-
-  console.log(newComment);
-}
-
-async function editCommentApi(commentId, commentVersion, post) {
-  const updatedPost = {
-    id: commentId,
-    post: post,
-    commentVersion: commentVersion,
-  };
-  const { data: updatedComment, errors } = await client.models.Comment.update(updatedPost)
-
-  console.log(updatedComment);
-}
-
-async function deleteCommentApi(commentId, commentVersion) {
-  const toBeDeletedTodo = {
-    id: commentId
-  }
-  
-  const { data: deletedComment, errors } = await client.models.Comment.delete(toBeDeletedTodo)
-
-  console.log(deletedComment);
 }
 
 export { Comments };

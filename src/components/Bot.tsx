fee2ed493472f53f1a1e331f310ff88ad364ba69
Riza Saputra <rizputra@amazon.com>
2024-11-29 00:43:35 +0700

    remove chat window

--- src/components/Bot.tsx
+++ src/components/Bot.tsx
@@ -1,27 +1,8 @@
-import { useState } from 'react';
-import {
-  Container, Header, SpaceBetween, TextContent, Button, Box
-} from "@cloudscape-design/components";
-import ChatBubble from "@cloudscape-design/chat-components/chat-bubble";
-import LoadingBar from "@cloudscape-design/chat-components/loading-bar";
-import Avatar from "@cloudscape-design/chat-components/avatar";
-import PromptInput from "@cloudscape-design/components/prompt-input";
-
-import { generateClient } from 'aws-amplify/data';
-import { Schema } from '../../amplify/data/resource';
-
-const client = generateClient<Schema>();
-export function Bot(props: any) {
-  type chat = {
-    role: string;
-    text: string;
-  };
-
-  const [chats, setChats] = useState<chat[]>([]);
-  const [value, setValue] = useState("");
-  const [isDisabled, setIsDisabled] = useState(false);
-
+export function Bot() {
   return (
-    <div>Ask AI assistant</div>
+    <div>
+      <h3>Ask AI assistant</h3>
+      <p>To be implemented...</p>
+    </div>
   );
 }
